f711ddf809b70cd304df6653ff1ec05bc091b697
Emilio Blanco <emilioeduardob@gmail.com>
2013-04-16 11:17:43 -0400

    Changed stream API to use geoJSON

--- app/serializers/stream_serializer.rb
+++ app/serializers/stream_serializer.rb
@@ -1,21 +1,27 @@
 class StreamSerializer < ActiveModel::Serializer
-  attributes :id, :url, :title, :desc, :lat, :lng, :geo_reference, :started_on, :channels
+  attributes :id, :url, :title, :desc, :started_on, :channels, :type, :properties, :geometry
   self.root = false
+
+
+  #to make valid geoJSON
+  def type
+    "feature"
+  end
+  
+  def geometry
+    {"coordinates" => [object.lat.to_f, object.lng.to_f], "type" => "Point"}
+  end
+
+  def properties
+    {"geo_reference" => object.geo_reference}
+  end
+
 
   def channels
     object.channels.map(&:id)
   end
 
-  #convert BigDecimal -> Float
-  def lat
-    object.lat.to_f
-  end
-
-  def lng
-    object.lng.to_f
-  end
-
-  #format this column
+  #format this date
   def started_on
     object.started_on.to_s(:api)
   end

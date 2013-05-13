65453ff9546891f4fedfd08d5bd91d04d100619d
Juan Ignacio Donoso <jidonoso@gmail.com>
2013-05-13 13:43:29 -0400

    Adds deployment stuff

--- Gemfile
+++ Gemfile
@@ -32,4 +32,9 @@ group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
+  gem 'therubyracer', :platforms => :ruby
+end
+
+group :production do
+  gem 'unicorn'
 end
---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+# Servers and their roles.
+server "church.peepol.tv", :web, :app, :db, primary: true
+
+# Server-side information.
+set :application, "peepoltv"
+set :user,        "deploy"
+set :deploy_to,   "/home/#{user}/applications/#{application}"
+
+# Repository (if any) configuration.
+set :deploy_via, :remote_cache
+set :repository, "https://github.com/peepoltv/peepoltv-api.git"
+set :branch,     "master"   # Optional, defaults to master
+# set :remote,   "negroku"      # Optional, defaults to origin
+# set :git_enable_submodules, 1
+
+# Web server configuration
+set :domains, "peepoltv.peepol.tv"
+
+# Database
+# set :migrate_env,    "migration"
+
+# Rails
+# set :rails_env, "production"
+
+# Unicorn
+set :unicorn_workers, 1

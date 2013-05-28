8a066df28af5f05d7c36041cddfb5af95d934ce1
Emilio Blanco <emilioeduardob@gmail.com>
2013-05-28 10:05:01 -0400

    Included friendly ID config in models

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -4,12 +4,15 @@ class Stream < ActiveRecord::Base
   before_create :setup_stream
   has_and_belongs_to_many :channels
 
+  extend FriendlyId
+  friendly_id :hash_token
+
   def self.by_channel(channel_id)
     Stream.joins(:channels).where("channel_id = ?", channel_id)
   end
 
   def setup_stream
-    self.id = Digest::MD5.hexdigest(self.inspect + Time.now.to_s)
+    self.hash_token = Digest::MD5.hexdigest(self.inspect + Time.now.to_s)
     self.started_on = Time.now
   end
 end

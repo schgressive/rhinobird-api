7051603a9e9c0c5e9b0370dab907f54f7e01067e
Emilio Blanco <emilioeduardob@gmail.com>
2013-06-17 12:57:19 -0400

    Added logging to nuve_hook and rescue methods

--- lib/nuve_hook.rb
+++ lib/nuve_hook.rb
@@ -12,18 +12,35 @@ module NuveHook
 
   #callback for room deletion
   def delete_licode_room
-    NUVE.deleteRoom(self.to_param)
+    Rails.logger.info "Deleting room #{self.to_param}"
+    begin
+      NUVE.deleteRoom(self.to_param)
+    rescue Exception => e
+      Rails.logger.debug "Error deleting room #{e.message}"
+    end
+
   end
 
   #assign room ID to stream hash_token
   def change_token
+    Rails.logger.info "creating room(#{self.title}) in Lynckia"
+    begin
+      response = NUVE.createRoom(self.title)
+    rescue Exception => e
+      response = '{}'
+      Rails.logger.debug "Error on create room: #{e.inspect}"
+    end
     #HOOK for NUVE
-    room = JSON.parse(NUVE.createRoom(self.title))
+    room = JSON.parse(response)
     self.hash_token = room["_id"]
   end
 
   #override token field on stream
   def generate_room_token
-    NUVE.createToken(self.to_param, "user#{Time.now.to_i}", "viewer")
+    begin
+      NUVE.createToken(self.to_param, "user#{Time.now.to_i}", "viewer")
+    rescue Exception => e
+      Rails.logger.debug "Error generating token for room: #{e.message}"
+    end
   end
 end

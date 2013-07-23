f1e7d207e983f23d8aa09451e43d54f025821380
Emilio Blanco <emilioeduardob@gmail.com>
2013-07-23 08:52:28 -0400

    Updated routing specs

--- spec/routing/channels_routing_spec.rb
+++ spec/routing/channels_routing_spec.rb
@@ -1,41 +1,44 @@
 require 'spec_helper'
 
 describe Api::ChannelsController do
+  route_prefix = "/api"
+  controller_prefix = "api/"
+
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/channels").to route_to("channels#index", format: :json)
+      expect(get: "#{route_prefix}/channels").to route_to("#{controller_prefix}channels#index", format: :json)
     end
 
     it "doesn't routes to #new" do
-      expect(get: "/channels/new").not_to route_to("channels#new")
+      expect(get: "#{route_prefix}/api/channels/new").not_to route_to("#{controller_prefix}channels#new")
     end
 
     it "routes to #streams" do
-      expect(get: "/channels/1/streams").to route_to("streams#index", channel_id: "1", format: :json)
+      expect(get: "#{route_prefix}/channels/1/streams").to route_to("#{controller_prefix}streams#index", channel_id: "1", format: :json)
     end
 
     it "routes to #update" do
-      expect(put: "/channels/1/streams/1").to route_to("streams#update", channel_id: "1", id: "1", format: :json)
+      expect(put: "#{route_prefix}/channels/1/streams/1").to route_to("#{controller_prefix}streams#update", channel_id: "1", id: "1", format: :json)
     end
 
     it "routes to #show" do
-      expect(get: "/channels/1").to route_to("channels#show", id: "1", format: :json)
+      expect(get: "#{route_prefix}/channels/1").to route_to("#{controller_prefix}channels#show", id: "1", format: :json)
     end
 
     it "doesn't routes to #edit" do
-      expect(get: "/channels/1/edit").not_to be_routable
+      expect(get: "#{route_prefix}/channels/1/edit").not_to be_routable
     end
 
     it "routes to #create" do
-      expect(post: "/channels").to route_to("channels#create", format: :json)
+      expect(post: "#{route_prefix}/channels").to route_to("#{controller_prefix}channels#create", format: :json)
     end
 
     it "doesn't routes to #update" do
-      expect(put: "/channels/1").not_to be_routable
+      expect(put: "#{route_prefix}/channels/1").not_to be_routable
     end
 
     it "routes to #destroy" do
-      expect(delete: "/channels/1").to route_to("channels#destroy", :id => "1", format: :json)
+      expect(delete: "#{route_prefix}/channels/1").to route_to("#{controller_prefix}channels#destroy", :id => "1", format: :json)
     end
 
   end

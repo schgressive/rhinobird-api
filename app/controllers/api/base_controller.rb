3d3e50406e801c484029d5fb541b75f9c8fd45c2
Emilio Blanco <emilioeduardob@gmail.com>
2013-07-24 10:35:29 -0400

    Using custom responder for API calls

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,4 +1,16 @@
 class Api::BaseController < ApplicationController
   respond_to :json
   before_filter :authenticate_user!
+
+  protected
+  def self.responder
+    Responders::JsonResponder
+    # lambda do |controller, resources, options|
+    #   if true
+    #     Responders::JsonResponder.call(controller, resources, options)
+    #   else
+    #     super.call(controller, resources, options)
+    #   end
+    # end
+  end
 end

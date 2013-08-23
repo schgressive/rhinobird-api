9293c77174a7ae2288582cca61812c627266f542
Emilio Blanco <emilioeduardob@gmail.com>
2013-08-23 10:24:45 -0400

    Add token to stream_pool

--- app/serializers/stream_pool_serializer.rb
+++ app/serializers/stream_pool_serializer.rb
@@ -1,6 +1,10 @@
 class StreamPoolSerializer < ActiveModel::Serializer
-  attributes :active
+  attributes :active, :token
   self.root = false
 
   has_one :stream
+
+  def token
+    object.user.vj_token
+  end
 end

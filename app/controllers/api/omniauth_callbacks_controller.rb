d560f6d7b26dbae40a0074bc52f275e104c36260
Emilio Blanco <emilioeduardob@gmail.com>
2014-01-17 15:54:54 -0300

    redirect to correct URL

--- app/controllers/api/omniauth_callbacks_controller.rb
+++ app/controllers/api/omniauth_callbacks_controller.rb
@@ -1,25 +1,25 @@
 class  Api::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    process_omniauth(request.env["omniauth.auth"], current_user)
+    process_omniauth(request.env["omniauth.auth"], current_user, "facebook")
   end
 
   def twitter
-    process_omniauth(request.env["omniauth.auth"], current_user)
+    process_omniauth(request.env["omniauth.auth"], current_user, "twitter")
   end
 
   def google_oauth2
-    process_omniauth(request.env["omniauth.auth"], current_user)
+    process_omniauth(request.env["omniauth.auth"], current_user, "google")
   end
 
   private
 
-  def process_omniauth(auth, user)
+  def process_omniauth(auth, user, from)
     @user = SocialSignup.new(auth, user).signup
 
     if @user.persisted?
       sign_in @user
     end
-    redirect_to "http://#{ENV["PUBLIC_HOST"]}"
+    redirect_to "http://#{ENV["PUBLIC_HOST"]}/profile/edit?complete=#{from}"
   end
 
 end

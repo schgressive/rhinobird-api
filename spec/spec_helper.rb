562a4e95dcbcdcd6f92b3dbac33b939b245deed2
Emilio Blanco <emilioeduardob@gmail.com>
2014-03-12 15:19:35 -0300

    support for focus test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,6 +36,11 @@ RSpec.configure do |config|
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
 
+
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)

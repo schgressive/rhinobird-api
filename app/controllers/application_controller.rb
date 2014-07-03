08c05076ac233590c139f34e8643a0a651c34148
Juan Ignacio Donoso <jidonoso@gmail.com>
2014-07-03 16:10:31 -0400

    chore(): Temporary remove protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  #protect_from_forgery
 
-  after_filter :set_csrf_cookie_for_ng
+  # after_filter :set_csrf_cookie_for_ng
 
-  def set_csrf_cookie_for_ng
-    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
-  end
+  # def set_csrf_cookie_for_ng
+  #   cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  # end
 
-  protected
-  def verified_request?
-    super || form_authenticity_token == request.headers['X_XSRF_TOKEN']
-  end
+  # protected
+  # def verified_request?
+  #   super || form_authenticity_token == request.headers['X_XSRF_TOKEN']
+  # end
 end

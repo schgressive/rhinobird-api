f563ef016ebfdfa64158cb3e08f31d5a604e3a62
Juan Ignacio Donoso <jidonoso@gmail.com>
2014-07-17 17:40:27 -0400

    chore(deploy): Change the nginx domain

--- config/deploy/beta.rb
+++ config/deploy/beta.rb
@@ -3,7 +3,7 @@ set :rails_env, :production
 
 set :application,   'rhinobird-api-beta'
 
-set :nginx_domains, "api.rhinobird.tv"
+set :nginx_domains, "api.beta.rhinobird.tv"
 set :branch, "beta"
 
 server 'church.rhinobird.tv', user: 'deploy', roles: %w{web app db}

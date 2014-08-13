353ceafb81a2be7ae60a817170876bef78dac666
Emilio Blanco <emilioeduardob@gmail.com>
2014-08-13 10:50:55 -0400

    Fix nuve. Real this time

--- lib/nuve_hook/stream.rb
+++ lib/nuve_hook/stream.rb
@@ -10,7 +10,7 @@ module NuveHook
       # returns the token if the stream is live
       def token
         new_token = nil
-        if self.created? || self.live?
+        if self.status.created? || self.status.live?
           new_token = generate_room_token
           # set live to false if unexisting room
           self.update_attributes(status: :pending) if new_token =~ /not exist/

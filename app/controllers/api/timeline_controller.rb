572a112d4186c9a58bc6bd86a59be0fb50ff87d2
Emilio Blanco <emilioeduardob@gmail.com>
2014-09-23 15:19:11 -0400

    feat(timeline): Hide created resources

--- app/controllers/api/timeline_controller.rb
+++ app/controllers/api/timeline_controller.rb
@@ -11,7 +11,7 @@ class Api::TimelineController < Api::BaseController
     @entries = @entries.includes(:resource => :user).order('promoted DESC, created_at DESC')
     @entries = @entries.page(params[:page] || 1)
     @entries = @entries.per(params[:per_page]) if params.key? :per_page
-    @entries = @entries.without_status(:pending)
+    @entries = @entries.without_status(:pending, :created)
 
     respond_with @entries
   end
--- spec/controllers/timeline_controller_spec.rb
+++ spec/controllers/timeline_controller_spec.rb
@@ -4,31 +4,35 @@ describe Api::TimelineController do
   describe "GET #index" do
 
     before do
-
+      # Create 2 timeline items
       Timecop.freeze(Time.now-20.seconds) do
-        @promoted_event = create(:stream, promoted:true)
+        @promoted_event = create(:live_stream, promoted:true)
       end
-
-      @vj_event = create(:vj)
-      @stream_event = create(:pending_stream)
-
-      Timecop.freeze(Time.now+10.seconds) do
-        @stream_event = create(:stream)
-      end
-
-      get :index, format: :json
-      @timelines = JSON.parse(response.body)
+      @vj_event = create(:vj, status: :archived)
     end
 
     it "returns success code" do
+      get :index, format: :json
       expect(response.status).to be(200)
     end
 
     it "returns correct content type" do
+      get :index, format: :json
       expect(response.header['Content-Type']).to include("application/json")
     end
 
-    it "returns an array of timelines with the promoted first" do
+    it "returns an array of timelines with the promoted first filtering created and pending" do
+      # shoud be ignored
+      @stream_event = create(:pending_stream)
+      @stream_created = create(:created_stream)
+
+      Timecop.freeze(Time.now+10.seconds) do
+        @stream_event = create(:live_stream)
+      end
+
+      get :index, format: :json
+      @timelines = JSON.parse(response.body)
+
       expect(@timelines).to have(3).items
       expect(@timelines[0]["resource_type"]).to eq("Stream")
       expect(@timelines[0]["resource"]["id"]).to eq(@promoted_event.to_param)

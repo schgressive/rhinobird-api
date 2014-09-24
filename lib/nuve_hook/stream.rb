23d875351c4d6b74e4fbf5e75e64abed2bbc5749
Emilio Blanco <emilioeduardob@gmail.com>
2014-09-24 15:40:08 -0400

    refactor(nuve.stream): Make code more clean

--- lib/nuve_hook/stream.rb
+++ lib/nuve_hook/stream.rb
@@ -5,17 +5,20 @@ module NuveHook
     included do
 
       before_destroy :delete_licode_room
-      before_create :change_token
+      before_create :setup_licode_room
 
-      # returns the token if the stream is live
+      # returns an access token if the room is live
       def token
-        new_token = nil
+        access_token = nil
         if self.status.created? || self.status.live?
-          new_token = generate_room_token
+          access_token = NuveHook::Nuve.create_access_token(self.to_param)
           # set live to false if unexisting room
-          self.update_attributes(status: :pending) if new_token =~ /not exist/
+          if access_token =~ /not exists/
+            self.update_attributes(status: :pending)
+            return nil
+          end
         end
-        new_token
+        access_token
       end
 
       # Refresh the flag if the room doesn't exist or the user list is empty
@@ -38,16 +41,11 @@ module NuveHook
       NuveHook::Nuve.delete_room(self.to_param)
     end
 
-    #assign room ID to stream hash_token
-    def change_token
+    # assign room ID to stream hash_token
+    def setup_licode_room
       room_name = "LicodeRoom-#{Digest::MD5.hexdigest(self.inspect + Time.now.to_s)}"
       room = NuveHook::Nuve.create_room(room_name)
       self.hash_token = room["_id"]
-    end
-
-    #override token field on stream
-    def generate_room_token
-      NuveHook::Nuve.create_access_token(self.to_param)
     end
 
   end

93abc9354e9f1727ef86aa363823ea355e00a9c0
Juan Ignacio Donoso <jidonoso@gmail.com>
2014-10-06 12:25:13 -0300

    chore(ssl): change the api domaing, to use the * wildcard certificate

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 ## BETA CONFIGURATION
 set :rails_env, :production
 
-set :nginx_domains, "api.beta.rhinobird.tv"
+set :nginx_domains, "api-beta.rhinobird.tv"
 set :branch, "production"
 
 server 'church.rhinobird.tv', user: 'deploy', roles: %w{web app db}
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 ## BETA CONFIGURATION
 set :rails_env, :production
 
-set :nginx_domains, "api.staging.rhinobird.tv"
+set :nginx_domains, "api-staging.rhinobird.tv"
 set :branch, "master"
 
 server 'godel.rhinobird.tv', user: 'deploy', roles: %w{web app db}

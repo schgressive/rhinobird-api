a41b87d8113ec4c79104dc28eaf6f4259c5eaac4
Emilio Blanco <emilioeduardob@gmail.com>
2015-01-15 09:53:48 -0300

    Fix user deletion spec

--- spec/services/user_deletion_service_spec.rb
+++ spec/services/user_deletion_service_spec.rb
@@ -7,18 +7,19 @@ RSpec.describe UserDeletionService, type: :Service do
   context "user data" do
     it "sets the user destruction time" do
       Timecop.freeze
-      UserDeletionService.new(user).run
-      expect(user.destruction_time).to eq(12.days.from_now)
+      UserDeletionService.new.perform(user.id, false)
+      user.reload
+      expect(user.destruction_time.to_i).to eq(12.days.from_now.to_i)
     end
 
     it "sets the user status as for_deletion" do
-      UserDeletionService.new(user).run
+      UserDeletionService.new.perform(user.id, false)
       user.reload
       expect(user.status).to eq "for_deletion"
     end
 
     it "deletes the user" do
-      UserDeletionService.new(user, purge: true).run
+      UserDeletionService.new.perform(user.id, true)
       expect{User.find(user.id)}.to raise_exception(ActiveRecord::RecordNotFound)
     end
   end
@@ -26,26 +27,26 @@ RSpec.describe UserDeletionService, type: :Service do
   context "related data" do
     it "marks streams for_deletion" do
       stream = create(:stream, user: user)
-      UserDeletionService.new(user).run
+      UserDeletionService.new.perform(user.id, false)
       stream.reload
       expect(stream.status).to eq "for_deletion"
     end
 
     it "deletes streams" do
       stream = create(:stream, user: user)
-      expect{UserDeletionService.new(user, purge: true).run}.to change{Stream.count}.by(-1)
+      expect{UserDeletionService.new.perform(user.id, true)}.to change{Stream.count}.by(-1)
       expect{Stream.find(stream.id)}.to raise_exception(ActiveRecord::RecordNotFound)
     end
 
     it "deletes vjs" do
       vj = create(:vj, user: user)
-      expect{UserDeletionService.new(user, purge: true).run}.to change{Vj.count}.by(-1)
+      expect{UserDeletionService.new.perform(user.id, true)}.to change{Vj.count}.by(-1)
       expect{Vj.find(vj.id)}.to raise_exception(ActiveRecord::RecordNotFound)
     end
 
     it "marks vjs for_deletion" do
       vj = create(:vj, user: user)
-      expect{UserDeletionService.new(user).run}.to change{Timeline.count}.by(-1)
+      expect{UserDeletionService.new.perform(user.id, false)}.to change{Timeline.count}.by(-1)
       vj.reload
       expect(vj.status).to eq "for_deletion"
     end

0962e89dcc0f269e8662e79d9fc913f883051203
Emilio Blanco <emilioeduardob@gmail.com>
2015-01-28 09:21:44 -0300

    fix(follow): Make follows public

--- app/controllers/api/followers_controller.rb
+++ app/controllers/api/followers_controller.rb
@@ -1,4 +1,5 @@
 class Api::FollowersController < Api::BaseController
+  skip_before_filter :authenticate_user!, only: [:index]
   def index
     @user = User.find(params[:user_id])
     @followers = @user.followers
--- app/controllers/api/following_controller.rb
+++ app/controllers/api/following_controller.rb
@@ -1,4 +1,5 @@
 class Api::FollowingController < Api::BaseController
+  skip_before_filter :authenticate_user!, only: [:index]
   def index
     @user = User.find(params[:user_id])
     @followed_users = @user.followed_users

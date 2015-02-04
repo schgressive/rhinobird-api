49e3669b1e4cf30287d532924946ca50c166a34c
Emilio Blanco <emilioeduardob@gmail.com>
2015-02-04 10:56:30 -0300

    chore(s3): Use https to serve assets

--- config/application.rb
+++ config/application.rb
@@ -82,6 +82,7 @@ module RhinobirdApi
     config.paperclip_defaults = {
       :storage => :s3,
       :bucket => ENV['AWS_BUCKET'],
+      :s3_protocol => :https,
       :s3_credentials => {
         :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
         :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY ']

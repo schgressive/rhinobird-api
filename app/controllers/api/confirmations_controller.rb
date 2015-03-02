3aec735831cefbf86c947f9f9c07327bc59436ac
Alvaro Gil <zevarito@gmail.com>
2015-03-02 17:09:02 -0200

    reponds with json to account confirmation

--- app/controllers/api/confirmations_controller.rb
+++ app/controllers/api/confirmations_controller.rb
@@ -1,5 +1,15 @@
 class Api::ConfirmationsController < Devise::ConfirmationsController
 
+  def create
+    self.resource = resource_class.send_confirmation_instructions(resource_params)
+
+    if successfully_sent?(resource)
+      render status: 200
+    else
+      render status: 400
+    end
+  end
+
   private
 
   def after_confirmation_path_for(resource_name, resource)
